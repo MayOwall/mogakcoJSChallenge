048f3a00e1103cb0a5f00025c45cb98a3fd2eb02
Owall <97934878+MayOwall@users.noreply.github.com>
2022-02-08 14:55:52 +0900

    day3
    모각코 3행시 만들기, 
    버튼과 onclick을 통한 innerHTML변환에 초점을 맞추어서 제작되었다.

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+function buttonMo() {
+  const button = document.getElementById("poemMo");
+  button.innerHTML = "두 모여서";
+};
+
+function buttonGak() {
+  const button = document.getElementById("poemGak");
+  button.innerHTML = "잡고 코딩하자,";
+};
+
+function buttonCo() {
+  const button = document.getElementById("poemCo");
+  button.innerHTML = "린이는 이제 안녕!";
+};
